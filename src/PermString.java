39c3c99cf79023e21d6a823d9ebf08fe0113b425
Yongnan (Devin) Li <devinli05@gmail.com>
2015-01-28 00:16:26 -0800

    added comments to functions

--- src/PermString.java
+++ src/PermString.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class PermString {
+	// find all permutations of a given string
 	public static ArrayList<String> getPerms(String s) {
 		ArrayList<String> permutations = new ArrayList<String>();
 		if (s == null)
@@ -23,12 +24,15 @@ public class PermString {
 		
 	}
 
+	// helper function that inserts a given character into a given string
+	// at the given location index.
 	private static String inserCharBefore(String word, char first, int j) {
 		String start = word.substring(0, j);
 		String end = word.substring(j);
 		return start + first + end;
 	}
 	
+	// the main method
 	public static void main(String [] args) {
 		String input = args[0];
 		ArrayList<String> output = getPerms(input);
